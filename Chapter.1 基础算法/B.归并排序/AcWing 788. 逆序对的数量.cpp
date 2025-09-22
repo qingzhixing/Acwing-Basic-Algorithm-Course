2347160b31ffa99b37e45131d86227da6bb39592
qingzhixing <qzsqqmail@qq.com>
2025-09-22 10:32:18 +0800

    🐱‍💻 AcWing 788. 逆序对的数量

--- Chapter.1 基础算法/B.归并排序/AcWing 788. 逆序对的数量.cpp
+++ Chapter.1 基础算法/B.归并排序/AcWing 788. 逆序对的数量.cpp
@@ -8,16 +8,18 @@ static const int MAX_N = (1e5 + 10);
 /*
  * 进行一次MergeSort我们有如下性质：
  *	array [range_left, range_right] 都是有序的.
+ * @return 逆序对的数量
  */
-void MergeSort(int array[], int range_left, int range_right)
+long long MergeSort(int array[], int range_left, int range_right)
 {
+	long long inverse_pair = 0;
 	if (range_left >= range_right)
-		return;
+		return inverse_pair;
 
 	int mid_index = (range_left + range_right) >> 1;
 
-	MergeSort(array, range_left, mid_index);
-	MergeSort(array, mid_index + 1, range_right);
+	inverse_pair += MergeSort(array, range_left, mid_index);
+	inverse_pair += MergeSort(array, mid_index + 1, range_right);
 
 	/*
 	 * 现在我们有如下性质:
@@ -51,6 +53,7 @@ void MergeSort(int array[], int range_left, int range_right)
 		else
 		{
 			temp[++pointer_temp] = array[pointer_right++];
+			inverse_pair += mid_index - pointer_left + 1;
 		}
 	}
 
@@ -59,6 +62,7 @@ void MergeSort(int array[], int range_left, int range_right)
 	{
 		array[range_left + i - 1] = temp[i];
 	}
+	return inverse_pair;
 }
 
 int main()
@@ -70,11 +74,7 @@ int main()
 		cin >> array[i];
 	}
 
-	MergeSort(array, 1, n);
-
-	for (auto i = 1; i <= n; i++)
-	{
-		cout << array[i] << ' ';
-	}
+	auto inverse_pair = MergeSort(array, 1, n);
+	cout << inverse_pair << std::endl;
 	return 0;
 }
