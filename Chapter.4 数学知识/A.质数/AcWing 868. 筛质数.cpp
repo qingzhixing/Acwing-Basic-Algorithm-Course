4c009be1b3de5a3bffc1e75bf21500a9e45313ef
qingzhixing <qzsqqmail@qq.com>
2026-08-03 22:18:18 +0800

    📦 NEW: AcWing 868. 筛质数

---
+++ Chapter.4 数学知识/A.质数/AcWing 868. 筛质数.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_N = 1e6 + 10;
+
+// 被 质数筛 是否筛掉
+bool filtered[MAX_N];
+int prime_count = 0;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	// 枚举 2 ~ n 所有数字
+	for (int number = 2; number <= n; number++)
+	{
+		if (filtered[number])
+		{
+			continue;
+		}
+		// 先前没被筛掉，那么number一定是质数
+		prime_count++;
+		// 用 number 筛选后面的数
+		int times = 2;
+		while (number * times <= n)
+		{
+			filtered[number * times] = true;
+			times++;
+		}
+	}
+
+	cout << prime_count << endl;
+	return 0;
+}
