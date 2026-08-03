10139b38d0a59dec73ff3ec9db08c4d219ff070d
qingzhixing <qzsqqmail@qq.com>
2026-08-03 22:28:42 +0800

    📦 NEW: 线性筛法

---
+++ Chapter.4 数学知识/A.质数/AcWing 868. 筛质数2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_N = 1e6 + 10;
+
+// 被 质数筛 是否筛掉
+bool filtered[MAX_N];
+int primes[MAX_N];
+int prime_count = 0;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	// 线性筛法
+	// 枚举 2 ~ n 所有数字
+	for (int number = 2; number <= n; number++)
+	{
+		if (!filtered[number])
+		{
+			// 先前没被筛掉，那么number一定是质数
+			primes[prime_count] = number;
+			prime_count++;
+		}
+
+		// 枚举当前所有质数进行筛选
+		for (int prime_idx = 0; primes[prime_idx] <= n / number; prime_idx++)
+		{
+			filtered[primes[prime_idx] * number] = true;
+			if (number % primes[prime_idx] == 0)
+			{
+				break;
+			}
+		}
+	}
+
+	cout << prime_count << endl;
+	return 0;
+}
