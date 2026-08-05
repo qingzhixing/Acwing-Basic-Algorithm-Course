b29ef9e61e1b2641f76d879fff6152f58b0e0f76
qingzhixing <qzsqqmail@qq.com>
2026-08-05 11:48:41 +0800

    📦 NEW: AcWing 871. 约数之和

---
+++ Chapter.4 数学知识/B. 约数/AcWing 871. 约数之和.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	unordered_map<int, int> primes;
+
+	while (n--)
+	{
+		int number;
+		cin >> number;
+
+		// 将 number 进行质因数分解
+
+		for (int i = 2; i <= number / i; i++)
+		{
+			int times = 0;
+			while (number % i == 0)
+			{
+				times++;
+				number /= i;
+
+				// 累加加入全局质因数分解表
+				primes[i]++;
+			}
+		}
+
+		if (number > 1)
+		{
+			primes[number]++;
+		}
+	}
+
+	long long result = 1;
+	for (auto [prime, times] : primes)
+	{
+		// 计算 [prime ^ 0 + ... + prime ^ times]
+		long long sum = 1;
+		while (times--)
+		{
+			sum = (prime * sum + 1) % MOD;
+		}
+		result = result * sum % MOD;
+	}
+	cout << result << endl;
+	return 0;
+}
