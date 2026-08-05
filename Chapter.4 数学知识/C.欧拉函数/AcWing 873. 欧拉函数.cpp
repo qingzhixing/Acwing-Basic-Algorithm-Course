cb3a750852d282be005ad96075768ed5db81569a
qingzhixing <qzsqqmail@qq.com>
2026-08-05 20:01:33 +0800

    📦 NEW: AcWing 873. 欧拉函数

---
+++ Chapter.4 数学知识/C.欧拉函数/AcWing 873. 欧拉函数.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// 质因数分解求欧拉函数
+int phi(int number)
+{
+	int result = number;
+	for (int i = 2; i <= number / i; i++)
+	{
+		if (number % i == 0)
+		{
+			result = result / i * (i - 1);
+			while (number % i == 0)
+			{
+				number /= i;
+			}
+		}
+	}
+	if (number > 1)
+	{
+		result = result / number * (number - 1);
+	}
+	return result;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		int number;
+		cin >> number;
+		cout << phi(number) << endl;
+	}
+	return 0;
+}
