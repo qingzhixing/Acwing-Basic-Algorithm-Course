a066c69a6302ee0749eade41bf3f728646642f71
qingzhixing <qzsqqmail@qq.com>
2026-08-05 20:52:33 +0800

    📦 NEW: AcWing 874. 筛法求欧拉函数

---
+++ Chapter.4 数学知识/C.欧拉函数/AcWing 874. 筛法求欧拉函数.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX_N = 1e6 + 10;
+
+// 当前已找出的质数
+vector<int> primes;
+
+// 已被筛掉的不是质数的数
+bool filtered[MAX_N];
+
+// phi[i] 表示 i 的 欧拉函数值
+int phi[MAX_N];
+
+long long eular_sum(int n)
+{
+	long long result = 0;
+
+	// 线性筛 求 欧拉函数
+	phi[1] = 1;
+	for (int number = 2; number <= n; number++)
+	{
+		// 未被筛掉，则说明是质数
+		if (!filtered[number])
+		{
+			primes.push_back(number);
+			// 根据欧拉函数性质，质数的 phi 为 它减去一
+			phi[number] = number - 1;
+		}
+
+		// 根据 number 筛后面的数
+		for (auto prime : primes)
+		{
+			// 越界，不需要继续筛了
+			if (prime * number > n)
+			{
+				break;
+			}
+
+			// prime 此时为 prime * number 的最小质因子
+			filtered[prime * number] = true;
+			// prime 为 number 最小质因子
+			if (number % prime == 0)
+			{
+				phi[number * prime] = phi[number] * prime;
+				break;
+			}
+			phi[number * prime] = phi[number] * (prime - 1);
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		result += phi[i];
+	}
+
+	return result;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << eular_sum(n) << endl;
+	return 0;
+}
