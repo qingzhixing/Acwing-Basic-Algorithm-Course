a7bedd0f79f033e673f56b2e0c3146a2f3433ff6
qingzhixing <qzsqqmail@qq.com>
2026-08-06 23:10:46 +0800

    📦 NEW: AcWing 877. 扩展欧几里得算法

---
+++ Chapter.4 数学知识/E.扩展欧几里得算法/AcWing 877. 扩展欧几里得算法.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int exgcd(int a, int b, int &x, int &y)
+{
+	if (b == 0)
+	{
+		x = 1;
+		y = 0;
+		return a;
+	}
+	int result = exgcd(b, a % b, y, x);
+	y -= a / b * x;
+	return result;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		int a, b, x, y;
+		cin >> a >> b;
+		exgcd(a, b, x, y);
+		printf("%d %d\n", x, y);
+	}
+	return 0;
+}
